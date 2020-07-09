4e66e7ac7486518e4520e9a4db2f90f6d85ca101
Oscar Giraldo <oscardan@utp.edu.co>
2020-07-09 17:34:49 -0500

    Inicializando estructura php

---
+++ Backend-Frontend-FullStack/perfil.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perfil Usuario</title>
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+    <div class="ctn-perfil">
+        <img src="" alt="">
+        <h1 class="title-welcome">Bienbenido a BandLab</h1>
+    </div>
+    
+</body>
+</html>
---
+++ Backend-Frontend-FullStack/regiser.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro de Usuarios</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    
+</body>
+
+ <!--contenedor de toda la estructura del proyect-->
+ <div class="container-all">
+    <!--container form - contenedor del formulario -->
+    <div class="ctn-form">
+        <img src="img/BandLab_logo.svg.png" alt="" class="log">
+        <!--<a title="pagina oficial" href="https://www.bandlab.com/"><img src="img/BandLab_logo.svg.png" alt="" class="log"></a>-->
+        <h1 class="title">Crear una Cuenta</h1>
+
+        <form action="">
+
+            <label for="">Nombre de Usuario</label>
+            <input type="text" name="" id="">
+            <span class="mensage-error"></span>
+
+            <label for="">Email</label>
+            <input type="email" name="" id="">
+            <span class="mensage-error"></span>
+
+            <label for="">Contraseña</label>
+            <input type="password" name="" id="">
+            <span class="mensage-error"></span>
+
+            <button type="submit">Registrar</button>
+            
+            <!--boton para iniciar sesión-->
+            <!--<input type="button" value="Registrar">-->
+            
+            <!--
+                <input type="submit" value="sss">
+            <input type="submit" value="Iniciar Sesión">
+            -->
+
+        </form>
+
+        <span class="text-footer">¿Aun no tienes una cuenta? 
+            <a href="">Registrate</a> 
+        </span>
+    </div>
+    
+    <!-- Contenedor del texto o descripcion de la plataforma -->
+    <div class="ctn-text">
+        <div class="capa"></div>
+        <h1 class="title-description">BandLab</h1>
+        <p class="text-description">
+            BandLab es la plataforma social de creación
+            de música fácil de usar todo en uno.
+            <br>
+            La plataforma en la nube donde músicos y fanáticos crean música, 
+            colaboran y se relacionan entre sí en todo el mundo.
+        </p>
+
+    </div>
+</div>
+</html>
